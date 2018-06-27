2ff2a986a3a340dbff79bcc6d9e7a6f8cf26b828
yiyun <yiyun@DESKTOP-IC037I0>
2018-06-28 07:46:13 +0800

    1.test

--- src/main/java/com/tim/fork/SpringbootApplication.java
+++ src/main/java/com/tim/fork/SpringbootApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
+	
 }
